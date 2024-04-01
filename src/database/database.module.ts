49f95c404c73f21a5772056623075c03642ab18c
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2024-04-01 22:40:45 +0100

    init telegram & DB

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { DatabaseService } from './database.service';
+import { DatabaseController } from './database.controller';
 
-@Module({})
+@Module({
+  providers: [DatabaseService],
+  controllers: [DatabaseController]
+})
 export class DatabaseModule {}
--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TelegramService } from './telegram.service';
+import { TelegramController } from './telegram.controller';
 
-@Module({})
+@Module({
+  providers: [TelegramService],
+  controllers: [TelegramController]
+})
 export class TelegramModule {}
